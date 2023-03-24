006159b6e96b318cd4f7eae3126b53cbee77ba95
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-24 17:26:25 +0000

    inital webpage code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates how much income tax you have to pay
+ */
+function myButtonClicked() {
+  // input
+  const hoursWorked = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyWage = parseFloat(document.getElementById("hourly-wage").value)
+
+  // process
+const pay = (hoursWorked * hourlyWage) * (1 - 0.18)
+const governmentPay = (hoursWorked * hourlyWage) * 0.18
+
+//output
+document.getElementById("your-pay").innerHTML = "You get $ " + pay.toFixed(2)
+document.getElementById("government-pay").innerHTML = "The government gets $" + governmentPay.toFixed(2)
+}
